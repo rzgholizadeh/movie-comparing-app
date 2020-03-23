00d02b8c41b20f522bb0f0e3d87ed645dce5bff1
Reza <rzgholizadeh@gmail.com>
2020-03-23 17:53:09 -0600

    feat: Implement single movie search

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hi there!');
+const fetchData = async searchTerm => {
+	const response = await axios.get("http://www.omdbapi.com/", {
+		params: {
+			apikey: "a42483f6",
+			s: searchTerm
+		}
+	});
+	console.log(response.data);
+};
+
+const input = document.querySelector("input");
+
+let timeoutId;
+const onInput = event => {
+	if (timeoutId) {
+		clearTimeout(timeoutId);
+	}
+	timeoutId = setTimeout(() => {
+		fetchData(event.target.value);
+	}, 1000);
+};
+input.addEventListener("input", onInput);
