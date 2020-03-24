ff3ed9d856c0be37352e9df4b058f65e66b2f7f0
Reza <rzgholizadeh@gmail.com>
2020-03-23 21:58:10 -0600

    feat: Add rendering of both searched movies

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const autocompleteConfig = {
             ${movie.Title} (${movie.Year})
         `;
 	},
-	onOptionSelect: movie => {
-		onMovieSelect(movie);
-	},
+
 	inputValue: movie => {
 		return movie.Title;
 	},
@@ -26,22 +24,30 @@ const autocompleteConfig = {
 
 createAutoComplete({
 	...autocompleteConfig,
-	root: document.querySelector("#left-autocomplete")
+	root: document.querySelector("#left-autocomplete"),
+	onOptionSelect: movie => {
+		document.querySelector(".tutorial").classList.add("is-hidden");
+		onMovieSelect(movie, document.querySelector("#left-summary"));
+	}
 });
 
 createAutoComplete({
 	...autocompleteConfig,
-	root: document.querySelector("#right-autocomplete")
+	root: document.querySelector("#right-autocomplete"),
+	onOptionSelect: movie => {
+		document.querySelector(".tutorial").classList.add("is-hidden");
+		onMovieSelect(movie, document.querySelector("#right-summary"));
+	}
 });
 
-const onMovieSelect = async movie => {
+const onMovieSelect = async (movie, summaryElement) => {
 	const response = await axios.get("http://www.omdbapi.com/", {
 		params: {
 			apikey: "a42483f6",
 			i: movie.imdbID
 		}
 	});
-	document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+	summaryElement.innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = movieDetail => {
