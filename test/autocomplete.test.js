f0ca992e8059d2c5d619ec811a3657adede2c90c
Reza <rzgholizadeh@gmail.com>
2020-04-02 17:53:03 -0600

    test: Write test for dropdown class name

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,3 +1,17 @@
-it("works!", ()=>{
-    
-})
+it("Shows an autocomplete", () => {
+	createAutoComplete({
+		root: document.querySelector("#target"),
+		fetchData() {
+			return [
+				{ Title: "Avengers" },
+				{ Title: "Scavengers" },
+				{ Title: "lavenders" }
+			];
+		},
+		renderOption(movie) {
+			return movie.Title;
+		}
+	});
+	const dropdown = document.querySelector(".dropdown");
+	expect(dropdown.className).not.to.include("is-active");
+});
